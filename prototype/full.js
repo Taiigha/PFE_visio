e7a10fc6ac9d4da7af8c3d8626707ecae52b1b16
Justine Camy <j.camy@outlook.fr>
2020-02-16 16:26:40 +0100

    Retrait de commentaires

--- prototype/full.js
+++ prototype/full.js
@@ -6,7 +6,6 @@
 //TODO prendre le flux audio et le vidéo à part
 var tracks = [];
 
-//https://webrtc.github.io/samples/src/content/peerpeerConnection1/trickle-ice/ pour tester les serveurs stun et turn
 var conf = {iceServers: [
               {url: "stun:stun.l.google.com:19302"}/*,
               {url: "turn:numb.viagenie.ca", credential: "webrtcdemo", username: "louis%40mozilla.com"}*/
@@ -20,18 +19,9 @@ var opt = {optional: [
 
 var peerConnection1 = new RTCPeerConnection(conf, opt);
 var peerConnection2 = new RTCPeerConnection(conf, opt);
-//var signalingChannel = new XMLHttpRequest();
 
 function gotStream(stream) {
   tracks = stream.getTracks();
-}
-
-function gotResponse(response) {
-  console.log(response);
-  /*if(response.sdp)
-    console.log(response.sdp);
-  else if(response.candidate)
-    console.log(response.candidate);*/
 }
 
 var promise = navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(gotStream);
@@ -41,9 +31,6 @@ peerConnection1.onicecandidate = function(e) {
     console.log("peerConnection1 localDescription");
     console.log(JSON.stringify(peerConnection1.localDescription));
     document.getElementById("offer").value = JSON.stringify(peerConnection1.localDescription);
-    /*if (signalingChannel.readyState === 1) {
-      signalingChannel.send(JSON.stringify(peerConnection1.localDescription));
-    }*/
   }
 }
 
@@ -52,9 +39,6 @@ peerConnection2.onicecandidate = function(e) {
     console.log("peerConnection2 localDescription");
     console.log(JSON.stringify(peerConnection2.localDescription));
     document.getElementById("answer").value = JSON.stringify(peerConnection2.localDescription);
-    /*if (signalingChannel.readyState === 1) {
-      signalingChannel.send(JSON.stringify(peerConnection1.localDescription));
-    }*/
   }
 }
 
@@ -71,10 +55,6 @@ function createOffer() {
   peerConnection1.createOffer().then(function(offer) {
     return peerConnection1.setLocalDescription(offer);
   });
-  /*signalingChannel.open("POST", "http://localhost:8000/"); //TODO fermer la connexion
-  signalingChannel.setRequestHeader("Access-Control-Allow-Origin", "*");
-  signalingChannel.setRequestHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-  signalingChannel.setRequestHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");*/
 }
 
 function receivedOffer(){
@@ -92,10 +72,3 @@ function receivedAnswer(){
 
  peerConnection1.setRemoteDescription(JSON.parse(answer));
 }
-/*
-signalingChannel.onreadystatechange = function() {
-  console.log(signalingChannel.readyState);
-  if (signalingChannel.readyState === 4 && signalingChannel.responseText != "") {
-      gotResponse(JSON.parse(signalingChannel.responseText));
-    }
-}*/
