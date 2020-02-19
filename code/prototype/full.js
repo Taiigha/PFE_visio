e56b6432ca0d4eea3cf3aa77d80196674dcd2c3d
Justine Camy <j.camy@outlook.fr>
2020-02-19 18:11:20 +0100

    Retrait de deux fichiers d'un ancien prototype inutile (create_offer). Modification du code du prototype full pour le rendre propre et gérer les exceptions.

--- code/prototype/full.js
+++ code/prototype/full.js
@@ -1,151 +1,188 @@
-//TODO bien mettre toutes les fonctions de gestion d'erreurs etc
-//TODO uniformiser le code et les noms de variables
-//TODO mettre des console.log à chaque étapes ?
-//TODO create data channel ?
-//TODO Vérifier si plusieurs tracks.
-//TODO prendre le flux audio et le vidéo à part
 var tracks = [];
 
 var conf = {iceServers: [
               {url: "stun:stun.l.google.com:19302"}/*,
-              {url: "turn:numb.viagenie.ca", credential: "webrtcdemo", username: "louis%40mozilla.com"}*/
+              {url: "turn:numb.viagenie.ca", credential: "webrtcdemo", username: "louis%40mozilla.com"}*/ //TURN Server, uncomment if necessary. Usable for development purpose only.
               ]
             };
+
 var opt = {optional: [
             {DtlsSrtpKeyAgreement: true}
             ]
           };
 
-var peerConnection1 = new RTCPeerConnection(conf, opt);
-var peerConnection2 = new RTCPeerConnection(conf, opt);
+var local = new RTCPeerConnection(conf, opt);
+var remote = new RTCPeerConnection(conf, opt);
 
 var dataChannel1 = null, isDataChannel1Open = false;
 var dataChannel2 = null, isDataChannel2Open = false;
 
-function gotStream(stream) {
-  //console.log(stream.getTracks());
-  tracks = stream.getTracks();
-}
-
-navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(gotStream); //TODO à retirer intelligement
-
-peerConnection1.onicecandidate = function(e) {
+//Events
+local.onicecandidate = function(e) {
   //console.log(e.candidate);
   if(e.candidate == null) {
-    console.log("peerConnection1 localDescription");
-    //console.log(JSON.stringify(peerConnection1.localDescription));
-    document.getElementById("offer").value = JSON.stringify(peerConnection1.localDescription);
+    console.log("offer done");
+    //console.log(JSON.stringify(local.localDescription));
+    document.getElementById("offer").value = JSON.stringify(local.localDescription);
   }
 }
 
-peerConnection2.onicecandidate = function(e) {
+remote.onicecandidate = function(e) {
   //console.log(e.candidate);
   if(e.candidate == null)  {
-    console.log("peerConnection2 localDescription");
-    //console.log(JSON.stringify(peerConnection2.localDescription));
-    document.getElementById("answer").value = JSON.stringify(peerConnection2.localDescription);
+    console.log("answer done");
+    //console.log(JSON.stringify(remote.localDescription));
+    document.getElementById("answer").value = JSON.stringify(remote.localDescription);
   }
 }
 
-//DEBUG
-peerConnection1.onconnectionstatechange = function(e) {
-  // console.log(peerConnection1.connectionState);
-  if(peerConnection1.connectionState === "connected")
+local.onconnectionstatechange = function(e) {
+  // console.log(local.connectionState);
+  if(local.connectionState === "connected")
     console.log("pc1 connected")
 }
 
-peerConnection2.onconnectionstatechange = function(e) {
-  // console.log(peerConnection2.connectionState);
-  if(peerConnection2.connectionState === "connected")
+remote.onconnectionstatechange = function(e) {
+  // console.log(remote.connectionState);
+  if(remote.connectionState === "connected")
     console.log("pc2 connected")
 }
 
-peerConnection1.oniceconnectionstatechange = function(e) {
-  // console.log(peerConnection1.iceConnectionState);
-  if(peerConnection1.iceConnectionState === "connected")
+local.oniceconnectionstatechange = function(e) {
+  // console.log(local.iceConnectionState);
+  if(local.iceConnectionState === "connected")
     console.log("pc1 connected (ice)")
 }
 
-peerConnection2.oniceconnectionstatechange = function(e) {
-  // console.log(peerConnection2.iceConnectionState);
-  if(peerConnection2.iceConnectionState === "connected")
+remote.oniceconnectionstatechange = function(e) {
+  // console.log(remote.iceConnectionState);
+  if(remote.iceConnectionState === "connected")
     console.log("pc2 connected (ice)")
 }
-//END DEBUG
+//END Events
 
-peerConnection1.ontrack = function(e) {
-  //console.log(e);
-  var audio = document.getElementById('audio');
-  audio.srcObject = e.streams[0];
-  audio.play();
+function sendMessage(){
+  var text = document.getElementById("textToSend").value;
+
+  showMessage(text, "local"); //TODO username
+
+  document.getElementById("textToSend").value = "";
+
+  if (isDataChannel1Open) { //TODO Peut on envoyer sur un seul channel ?
+    dataChannel1.send(text);
+  }
+  if (isDataChannel2Open) {
+    dataChannel2.send(text);
+  }
 }
 
-peerConnection2.ontrack = function(e) {
-  //console.log(e);
-  var audio = document.getElementById('audio');
-  audio.srcObject = e.streams[0];
-  audio.play();
+function showMessage(data, username) {
+  var date = new Date();
+  var log = document.createElement('p');
+  var logValue = document.createTextNode(username + "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] : " + data);
+
+  log.appendChild(logValue);
+  document.getElementById("chatbox").appendChild(log);
 }
 
-function createOffer() { //DIFF Datachannel
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(stream) {
-    peerConnection1.addTrack(tracks[0]);
-    dataChannel1 = peerConnection1.createDataChannel("dc1", {negotiated: true, id: 0});
-    dataChannel1.onopen= function(event) {
-      dataChannel1.send('Hi you!');
-      isDataChannel1Open = true;
-    }
-    dataChannel1.onmessage = function(event) {
-      //console.log(event.data);
-      showMessage(event.data, "pc2");
-    }
-    peerConnection1.createOffer().then(function(offer) {
-      return peerConnection1.setLocalDescription(offer);
+function testDevices(callback) {
+  var isAudioAvailable = false, isVideoAvailable = false;
+  //Test if audio and video devices are availables
+  navigator.mediaDevices.enumerateDevices().then(function(devices) {
+    devices.forEach(function(device) {
+      if(device.kind === "audioinput")
+        isAudioAvailable = true;
+      if(device.king === "videoinput")
+        isVideoAvailable = true;
     });
+    callback(isAudioAvailable, isVideoAvailable);
+  })
+  .catch(function(err) {
+    console.log(err.name + " : " + err.message);
   });
 }
 
-function receivedOffer(){
+function setUpDataChannel(dataChannel, username){
+  dataChannel.onopen= function(event) {
+    dataChannel.send(username + " connected to chatbox");
+    isDataChannel1Open = true;
+  }
+
+  dataChannel.onmessage = function(event) {
+    showMessage(event.data, username);
+  }
+}
+
+function setUpAudio(peer) {
+  peer.ontrack = function(e) {
+    audio.srcObject = e.streams[0];
+    audio.play();
+  }
+}
+
+function createOffer(isAudioAvailable, isVideoAvailable) {
+  navigator.mediaDevices.getUserMedia({ audio: isAudioAvailable, video: isVideoAvailable }).then(function(stream) {
+    //Retrieve tracks
+    tracks =  stream.getTracks();
+    local.addTrack(tracks[0]); //TODO regarder comment ça fonctionne avec video
+
+    //Create datachannel
+    dataChannel1 = local.createDataChannel("dc1", {negotiated: true, id: 0});
+    setUpDataChannel(dataChannel1, "local");
+
+    setUpAudio(local);
+
+    //Create offer
+    local.createOffer().then(function(offer) {
+      return local.setLocalDescription(offer).catch(function (err) {
+        console.log(err.name + " : " + err.message);
+      });
+    }).catch(function(err) {
+      console.log(err.name + " : " + err.message);
+    });
+
+  }).catch(function(err) {
+    console.log(err.name + " : " + err.message);
+  });
+}
+
+function receivedOffer(isAudioAvailable, isVideoAvailable){
   var offer = document.getElementById("offer").value;
 
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(stream) {
-    peerConnection2.addTrack(tracks[0]);
-    dataChannel2 = peerConnection2.createDataChannel("dc2", {negotiated: true, id: 0});
-    dataChannel2.onopen= function(event) {
-      dataChannel2.send('Hi you!');
-      isDataChannel2Open = true;
-    }
-    dataChannel2.onmessage = function(event) {
-      //console.log(event.data);
-      showMessage(event.data, "pc2");
-    }
-    peerConnection2.setRemoteDescription(new RTCSessionDescription(JSON.parse(offer)));
-    peerConnection2.createAnswer().then(function(answer) {
-      peerConnection2.setLocalDescription(answer);
+  navigator.mediaDevices.getUserMedia({ audio: isAudioAvailable, video: isVideoAvailable }).then(function(stream) {
+    //Retrieve tracks
+    tracks = stream.getTracks();
+    remote.addTrack(tracks[0]);
+
+    //Create datachannel
+    dataChannel2 = remote.createDataChannel("dc2", {negotiated: true, id: 0});
+    setUpDataChannel(dataChannel2, "remote");
+
+    setUpAudio(remote);
+
+    //Save remote offer
+    remote.setRemoteDescription(new RTCSessionDescription(JSON.parse(offer))).catch(function (err) {
+      console.log(err.name + " : " + err.message);
     });
+
+    //Create answer
+    remote.createAnswer().then(function(answer) {
+      remote.setLocalDescription(answer).catch(function (err) {
+        console.log(err.name + " : " + err.message);
+      });
+    }).catch(function(err) {
+      console.log(err.name + " : " + err.message);
+    });
+
+  }).catch(function(err) {
+    console.log(err.name + " : " + err.message);
   });
 }
 
 function receivedAnswer(){
  var answer = document.getElementById("answer").value;
 
- peerConnection1.setRemoteDescription(new RTCSessionDescription(JSON.parse(answer)));
-}
-
-function sendMessage(){
-  showMessage(document.getElementById("textToSend").value, "pc1");
-  if (isDataChannel1Open) { //Peut on envoyer sur un seul channel ?
-    dataChannel1.send(document.getElementById("textToSend").value);
-  }
-  if (isDataChannel2Open) {
-    dataChannel2.send(document.getElementById("textToSend").value);
-  }
-}
-
-function showMessage(data, pc) {
-  var date = new Date();
-  var log = document.createElement('p');
-  var logValue = document.createTextNode(pc + "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "] : " + data);
-  log.appendChild(logValue);
-  document.getElementById("chatbox").appendChild(log);
+ local.setRemoteDescription(new RTCSessionDescription(JSON.parse(answer))).catch(function (err) {
+   console.log(err.name + " : " + err.message);
+ });
 }
